dc2d07529a89d3a113894b04209cfcd72d95dc9f
defendend <ivsacha@mail.ru>
2025-12-08 22:22:27 +0300

    Fix

--- desktop/src/main/kotlin/org/example/di/AppModule.kt
+++ desktop/src/main/kotlin/org/example/di/AppModule.kt
@@ -10,7 +10,7 @@ import org.koin.dsl.module
  */
 val appModule = module {
     // Network layer
-    singleOf(::ChatApiClient)
+    single { ChatApiClient() }
 
     // UI layer
     single { ChatViewModel(get()) }
